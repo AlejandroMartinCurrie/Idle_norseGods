717b6ef5f95944e09f62759d7ed3a1e8c8165190
AlejandroMartinCurrie <gatoibiza1@hotmail.com>
2021-05-05 14:54:05 +0200

    creates stages scriptable object

---
+++ Assets/Scripts/Stages.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Stage", menuName = "Create Stage")]
+public class Stages : ScriptableObject
+{
+
+    public Boss stageBoss;
+    public Enemies stageEnemy;
+    public Sprite backgroundImage;
+    
+}
